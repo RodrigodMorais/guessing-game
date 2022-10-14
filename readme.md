4221e6ebdadd94401f49fabe4a08153419efadf1
Rodrigo de Morais <101508965+RodrigodMorais@users.noreply.github.com>
2022-10-13 21:08:14 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,38 @@
+# 💻 Projeto: Guessing game (Jogo da adivinhação)
+
+<img src="images/guessing-game-print.png">
+
+<br>
+
+## 📌 Sobre o Projeto
+
+Projeto desenvolvido no programa Explorer da Rocketseat.
+Assuntos importantes aplicados: 
+
+  * Funções - Js
+  * Manipulação da DOM
+
+Figma: https://www.figma.com/file/I3sT97Beg95aeV3KfHKh8P/Jogo-Adivinha%C3%A7%C3%A3o-(Copy)?node-id=0%3A1
+
+<br>
+
+## 📌 Tecnologias 
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+* HTML
+* CSS
+* JavaScript
+
+<br>
+
+## 📝 Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
+<br>
+<br>
+
+<h4 align="center">
+    Por Rodrigo de Morais 🚀
+</h4>
---
+++ scripts.js
@@ -0,0 +1,40 @@
+const screen1 = document.querySelector(".screen-01")
+const screen2 = document.querySelector(".screen-02")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+// Events
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
+})
+
+function handleTryClick(event) {
+    event.preventDefault() 
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == randomNumber) {
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `Go it right in ${xAttempts} tries!`
+    }
+
+    inputNumber.value = ""
+    xAttempts++
+}
+
+function handleResetClick() {
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
